4f541239cd1b6da411abb3133e5d6514d76bf938
Jason Bornhoft <jmvbxx@gmail.com>
2024-01-01 16:15:19 -0500

    Add comments for hello-buffer

--- hello-buffer.py
+++ hello-buffer.py
@@ -2,8 +2,10 @@
 from bcc import BPF
 
 program = r"""
+// Create a BPF table for pushing out custom event data
 BPF_PERF_OUTPUT(output);
 
+// Create a struct with the following
 struct data_t {
     int pid;
     int uid;
@@ -12,15 +14,26 @@ struct data_t {
 };
 
 int hello(void *ctx) {
+    // Initialize a `data_t` struct named `data`
     struct data_t data = {};
-    char message[12] = "Hello, World!";
+    // Create a character array `message`
+    char message[12] = "Hello World!";
 
+    // Retrieve current process id (shifted to the right 32 bits) 
+    // and assign to `data.pid`
     data.pid = bpf_get_current_pid_tgid() >> 32;
+    // Get the user ID that is running the process that triggered
+    // the kprobe event. The user ID is held in the lowest 32 bits
+    // of the 64-bit value that gets returned
     data.uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
 
+    // Populate the first argument address with the current process name
     bpf_get_current_comm(&data.command, sizeof(data.command));
+    // The message "Hellow World!" gets copied into the correct place
+    // within the message
     bpf_probe_read_kernel(&data.message, sizeof(data.message), message);
 
+    // Place the data within the map
     output.perf_submit(ctx, &data, sizeof(data));
 
     return 0;
